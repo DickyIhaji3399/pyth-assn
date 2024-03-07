76905af7d2a84f2daf40ad8ac67dcaf056fc4af7
DickyIhaji3399 <160227165+DickyIhaji3399@users.noreply.github.com>
2024-03-08 00:39:13 +0300

    Initial commit

---
+++ assn.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Assign specific values for name, age, and location
+name = "ihaji"
+age = 26
+location = "Nairobi"
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
